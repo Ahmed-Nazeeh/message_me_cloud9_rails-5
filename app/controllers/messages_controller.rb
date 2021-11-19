b0457a8dcaf4ec297efd475d1e68ad241b1702f3
Ubuntu <ubuntu@ip-172-31-33-17.us-east-2.compute.internal>
2021-11-19 06:22:53 +0000

    add auto scroll to chat box

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,8 @@ class MessagesController < ApplicationController
       if message.save
          #redirect_to root_path  #what we want to do is to broadcast this message
          ActionCable.server.broadcast "chatroom_channel",
-                                    foo: message.body #it take the form of hash and riecived by the data in the chatroom.coffee.js
+                                   # foo: message.body #it take the form of hash and riecived by the data in the chatroom.coffee.js
+                                    mod_message: message_render(message)
       end
    end
    
@@ -14,6 +15,10 @@ class MessagesController < ApplicationController
    
    def message_params
       params.require(:message).permit(:body) 
+   end
+   
+   def message_render(message)
+      render(partial: 'message', locals: {message: message})
    end
    
 end
