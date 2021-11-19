fc8a078ac50f7b2dcdfc3fe5ae83dcfbd9c5048a
Ubuntu <ubuntu@ip-172-31-33-17.us-east-2.compute.internal>
2021-11-19 14:37:49 +0000

    heroku configuration 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.web_socket_server_url = "wss://action-cable-MessageMe.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://action-cable-MessageMe.herokuapp.com', 'http://action-cable-MessageMe.herokuapp.com']
+  config.web_socket_server_url = "wss://messagee-mee.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://messagee-mee.herokuapp.com', 'http://action-cable-message-mee-app.herokuapp.com']
 end
